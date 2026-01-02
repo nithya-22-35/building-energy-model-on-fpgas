8d6725637acd4d076f9af4037332488ba64e6c02
nithya-22-35 <nithyasetty22@gmail.com>
2026-01-02 15:58:02 +0530

    Update anomaly_detector_tb.cpp

--- fpga/anomaly_detector_tb.cpp
+++ fpga/anomaly_detector_tb.cpp
@@ -1,1 +1,18 @@
 #code
+#include <iostream>
+#include "model_params.h"
+
+void anomaly_detector(float, float, float, int*);
+
+int main() {
+    int anomaly;
+
+    anomaly_detector(500.0, 200.0, 50.0, &anomaly);
+
+    if (anomaly)
+        std::cout << "⚠️ ALERT: Abnormal energy usage detected!" << std::endl;
+    else
+        std::cout << "Normal usage" << std::endl;
+
+    return 0;
+}
