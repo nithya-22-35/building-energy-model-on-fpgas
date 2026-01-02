931485149689ca55ed4c58b865006f7a2f5a5fe9
nithya-22-35 <nithyasetty22@gmail.com>
2026-01-02 15:57:17 +0530

    Update anomaly_detector.cpp

--- fpga/anomaly_detector.cpp
+++ fpga/anomaly_detector.cpp
@@ -1,1 +1,17 @@
 #code
+#include "model_params.h"
+
+void anomaly_detector(float energy, float mean, float std, int *anomaly) {
+#pragma HLS INTERFACE s_axilite port=return
+#pragma HLS INTERFACE s_axilite port=energy
+#pragma HLS INTERFACE s_axilite port=mean
+#pragma HLS INTERFACE s_axilite port=std
+#pragma HLS INTERFACE s_axilite port=anomaly
+
+    float z_score = (energy - mean) / std;
+
+    if (z_score > ANOMALY_THRESHOLD)
+        *anomaly = 1;
+    else
+        *anomaly = 0;
+}
